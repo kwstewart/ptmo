8f7db3fdf691d6e96c18783882895d74d8394728
Keith Stewart <keith@giantsource.com>
2017-05-25 19:13:19 -0400

    Allowed hosts and Installed apps

--- ptmo/ptmo/settings.py
+++ ptmo/ptmo/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ptmo.tropicalwebdev.net']
 
 
 # Application definition
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myfarog'
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
