bf1f6ccb7e6f5521171225fc3680630565420393
Keith Stewart <keith@giantsource.com>
2017-05-25 20:27:58 -0400

    Models and admin

---
+++ ptmo/ptmo/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+
+from models import *
+
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slack_channel')
+    search_fields = ['name']
+    save_as = True
+
+admin.site.register(Location, LocationAdmin)
+
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('location', 'name', 'text')
+    search_fields = ['name']
+    save_as = True
+admin.site.register(Room, RoomAdmin)
+
+
+class DoorAdmin(admin.ModelAdmin):
+    list_display = ('curr_room', 'dest_room', 'name', 'text','locked')
+    search_fields = ['name']
+    save_as = True
+admin.site.register(Door, DoorAdmin)
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('curr_room', 'name', 'text','locked')
+    search_fields = ['name']
+    save_as = True
+admin.site.register(Item, ItemAdmin)
---
+++ ptmo/ptmo/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+class Location(models.Model):
+    name                = models.CharField(max_length=64)
+    slack_channel   =  models.CharField(max_length=16)
+    
+    class Meta:
+        verbose_name = "Location"
+        verbose_name_plural = "Locations"
+
+
+class Room(models.Model):
+    location    = models.ForeignKey(location)
+    name                = models.CharField(max_length=64)
+    text               = models.CharField(max_length=255)
+    
+    class Meta:
+        verbose_name = "Room"
+        verbose_name_plural = "Rooms"
+
+
+class Door(models.Model):
+    curr_room    = models.ForeignKey(room)
+    dest_room    = models.ForeignKey(room)
+    name                = models.CharField(max_length=64)
+    text                = models.CharField(max_length=16)
+    locked      = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "Door"
+        verbose_name_plural = "Doors"
+
+
+class Item(models.Model):
+    curr_room    = models.ForeignKey(room)
+    name                = models.CharField(max_length=64)
+    text                = models.CharField(max_length=16)
+    locked      = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "Door"
+        verbose_name_plural = "Doors"
+
+
